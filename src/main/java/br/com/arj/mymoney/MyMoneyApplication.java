3eeb1a50f86bc6636f09af3a191d4e7e1f1bb111
alvarinojr <alvarinojr@gmail.com>
2020-12-20 21:35:22 -0300

    Commit Inicial

---
+++ src/main/java/br/com/arj/mymoney/MyMoneyApplication.java
@@ -0,0 +1,13 @@
+package br.com.arj.mymoney;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyMoneyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyMoneyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/arj/mymoney/MyMoneyApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.arj.mymoney;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyMoneyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
